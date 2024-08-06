7610b6b8021b45e0cb4087b80dae6120e6135ae6
caleb dennis <caleb.dennis429@gmail.com>
2024-08-06 15:23:35 -0600

    Permute and determinant algorithms

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 
 mod matrix;
 mod graph;
+mod permute;
 use matrix::{Matrix};
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,17 +1,15 @@
 use core::{cmp::PartialEq, ops::{Mul, Add, Sub, Div, Deref, Neg}};
 use std::{array::TryFromSliceError};
 use std::mem::MaybeUninit;
+use crate::permute::Permuter;
+use num::traits::{Zero, One};
 
-
-
-// pub struct Matrix <T, const N: usize>{
-//     // TODO: store row or column order, convert only as needed
-//     m: [[T; N]; N]
-// }
-
-pub struct Matrix <T,  M: Deref<&[T]>> {
-    m: M
+pub struct Matrix <T, const N: usize>{
+    // TODO: store row or column order, convert only as needed
+    m: [[T; N]; N]
 }
+
+pub trait Ring: Mul<Output = Self> + Div<Output = Self> + Sub<Output = Self> + Add<Output = Self> + Zero + One + Neg<Output = Self> {}
 
 pub trait Determinant<T> {
     fn det(&self) -> T;
@@ -116,29 +114,32 @@ impl<T: PartialEq, const N: usize> PartialEq for Matrix<T, N> {
     }
 }
 
-impl<T: Mul<Output = T> + Sub<Output = T> + Add<Output = T> + Copy, const N: usize> Determinant<T> for Matrix<T, N> {
+impl<T: Ring + Copy, const N: usize> Determinant<T> for Matrix<T, N> {
 
     // TODO: fast Determinant?
     fn det(&self) -> T {
-        fn naive_det(&self) -> T {
-            let mut sum = self.m[0][0];
-            for i in 0..N{
-                self.m[0][i] *
-            }
-            sum
-        }
-
         match N {
             1 => self[0][0],
             2 => self[0][0] * self[1][1] - self[0][1] * self[1][0],
-            _ => unimplemented!(),
+            _ => naive_det(&self.m),
         }
     }
-
-
 }
 
-impl<T: Mul<Output = T> + Div<Output = T> + Neg<Output = T> + Sub<Output = T> + Copy, const N: usize> Inverse<T> for Matrix<T, N> {
+fn naive_det<T, const N: usize>(m: &[[T; N]; N]) -> T
+where T: Ring + Copy
+{
+    let mut permutations = Permuter::new(m.len());
+    let mut sum = T::zero();
+    let mut even_odd = T::one();
+    while let Some(p) = permutations.next() {
+        sum = sum + even_odd * p.iter().enumerate().map(|(i, j)| m[i][*j]).fold(T::zero(), |product, a| product * a);
+        even_odd = -even_odd;
+    }
+    sum
+}
+
+impl<T: Ring + Copy, const N: usize> Inverse<T> for Matrix<T, N> {
     fn inv(&self) -> Self {
         match N {
             1 => (&[&[self[0][0] / self[0][0] / self[0][0]][0..]][0..]).try_into().expect(""),
@@ -148,10 +149,14 @@ impl<T: Mul<Output = T> + Div<Output = T> + Neg<Output = T> + Sub<Output = T> + 
     }
 }
 
+fn gray_code(n: u32) -> u32 {
+    n ^ (n >> 1)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    impl Ring for i32 {}
 
     #[test]
     fn test_eq() {
@@ -176,5 +181,10 @@ mod tests {
         let a: Matrix<i32, 2>= Into::<Matrix<i32, 2>>::into([[-2, -7], [1, 4]]);
         assert!(&a * &a.inv() == [[1,0],[0,1]].into());
         assert!(&a.inv() * &a == [[1,0],[0,1]].into());
+    }
+
+    #[test]
+    fn test_gray_code() {
+        assert_eq!((0..4).map(|n| gray_code(n)).collect::<Vec<u32>>(), &[0,1,3,2])
     }
 }
